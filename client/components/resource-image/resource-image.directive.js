fa73d2089237c3851ead2b93804a771e50a4090e
ndxbxrme <lewis_the_cat@hotmail.com>
2015-05-09 16:15:45 +0100

    resource-image

--- client/components/resource-image/resource-image.directive.js
+++ client/components/resource-image/resource-image.directive.js
@@ -5,7 +5,18 @@ angular.module('pApp')
     return {
       templateUrl: 'components/resource-image/resource-image.html',
       restrict: 'EA',
+      scope: {
+        image: '=resourceImage',
+        riText: '@',
+        size: '@'
+      },
+      replace: true,
       link: function (scope, element, attrs) {
+        scope.width = scope.height = 150;
+        if(scope.size) {
+          scope.width = scope.size.split('x')[0];
+          scope.height = scope.size.split('x')[1]; 
+        }
       }
     };
   });
